a88e3d4a0e45b0fb671420ec5dd33e407e35665c
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-01-30 10:22:59 -0600

    got rid of silly<p>

--- src/makeHTML.js
+++ src/makeHTML.js
@@ -85,7 +85,7 @@ function generateHTML(array) {
         </header>
         <div class = "card-deck" id="all-cards">
            
-          <p>${employeeCards(array)}</p>
+          ${employeeCards(array)}
       
         </div>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
