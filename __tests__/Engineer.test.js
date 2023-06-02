e0172ea708ce6f2fef7298065a47d8b1501ea91d
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-06-02 16:48:57 -0500

    fancying up the tests a bit

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,7 +2,10 @@ const { Engineer } = require('../lib');
 
 const name = "Constantine", email="go@way.com", id = 747, gitHub = 'constip';
 const engineer = new Engineer(name, id, email, gitHub);
+
+describe('Engineer Tests:', () =>{
 //getRole() should return 'Engineer' for Engineer class
-test('Testing .getRole() method', () => expect(engineer.getRole()).toBe('Engineer'));
+    it('Testing .getRole() method', () => expect(engineer.getRole()).toBe('Engineer'));
 //getGitHub() method should return 'GitHub handle string' for Engineer class
-test('Testing .getGitHub() method', () => expect(engineer.getGitHub()).toBe(gitHub));
+    it('Testing .getGitHub() method', () => expect(engineer.getGitHub()).toBe(gitHub));
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,7 +2,10 @@ const { Manager } = require('../lib');
 
 const name = "Michael Scott", email="Michael99@hotmail.com", id = 7, officeNumber = '11';
 const manager = new Manager(name, id, email, officeNumber);
+
+describe("Manager Tests:", () => {
 // .getRole() method should return 'Manager' for the Manager class
-test('Testing .getRole() method', () => expect(manager.getRole()).toBe('Manager'));
+  it('Testing .getRole() method', () => expect(manager.getRole()).toBe('Manager'));
 // .getOfficeNumber() method should return officeNumber 
-test('Testing .getOfficeNumber() method', () => expect(manager.getOfficeNumber()).toBe(officeNumber));
+  it('Testing .getOfficeNumber() method', () => expect(manager.getOfficeNumber()).toBe(officeNumber));  
+});
